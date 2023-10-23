e9dad395f30bcb100d652403034f09e697b7a6f1
akashydv311 <akashydv311@gmail.com>
2023-10-23 21:06:02 +0530

    Armstrong number code

---
+++ Coder Army/Day-19/classwork/ArmstrongNumber.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int coutNumberDig(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+bool isArmstrong(int num, int count)
+{
+
+    int ans = 1, rem, temp = num;
+    while (num)
+    {
+        rem = num % 10;
+        num /= 10;
+        ans = ans + pow(rem, count);
+        // cout << ans << endl;
+    }
+    if (ans == temp)
+        return true;
+    return false;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+    // cout number of digit
+    int count = coutNumberDig(num);
+    bool result = isArmstrong(num, count);
+    // cout << count << endl;
+    cout << num << " is armstrong number: " << boolalpha << result << endl;
+    return true;
+}
