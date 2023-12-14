aba738e64e6dc99e6a0be93f879481c938d489dc
akashydv311 <akashydv311@gmail.com>
2023-12-14 12:42:07 +0530

    Fiboassi series

--- Coder Army/Day-23/classwork/FibonassiSeries.cpp
+++ Coder Army/Day-23/classwork/FibonassiSeries.cpp
@@ -6,27 +6,22 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    // index number of fibonassi series
-    int index;
-    cin >> index;
+    //
+    int n;
+    cin >> n;
 
     // fibonassi series
-    int arr[index + 1] = {0};
+    int arr[1000];
 
     // fix element
     arr[0] = 0;
     arr[1] = 1;
 
     for (int i = 2; i <= index; i++)
-    {
         arr[i] = arr[i - 1] + arr[i - 2];
-    }
 
-    // printing series
-    for (int i = 0; i <= index; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    // find nth fibonassi
+    cout << arr[index];
 
     return 0;
 }
