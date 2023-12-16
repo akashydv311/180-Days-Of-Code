4e505b979bf0edcbb803272a4b619454b0ff9ea2
akashydv311 <akashydv311@gmail.com>
2023-12-16 13:44:21 +0530

    fibonasii series code

--- Coder Army/Day-23/classwork/FibonassiSeries.cpp
+++ Coder Army/Day-23/classwork/FibonassiSeries.cpp
@@ -17,11 +17,11 @@ int main()
     arr[0] = 0;
     arr[1] = 1;
 
-    for (int i = 2; i <= index; i++)
+    for (int i = 2; i <= n; i++)
         arr[i] = arr[i - 1] + arr[i - 2];
 
     // find nth fibonassi
-    cout << arr[index];
+    cout << arr[n];
 
     return 0;
 }
