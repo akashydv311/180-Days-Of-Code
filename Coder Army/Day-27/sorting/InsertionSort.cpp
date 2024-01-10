7b574c9d61fddba5cbc568033b182cffd9ba42c9
akashydv311 <akashydv311@gmail.com>
2024-01-10 08:13:40 +0530

    Day-27 insertion sort added

---
+++ Coder Army/Day-27/sorting/InsertionSort.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    // values in an array
+    cout << "Enter values in an array: " << endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // implimenting insertion sort
+
+    return 0;
+}
