98f189286c7cb69d72d9ed6f5904941550c07ccb
akashydv311 <akashydv311@gmail.com>
2024-01-10 06:32:07 +0530

    bubble sort algo assignment added

--- Coder Army/Day-26/assigenment/Soluction1.cpp
+++ Coder Army/Day-26/assigenment/Soluction1.cpp
@@ -14,5 +14,18 @@ int main()
         cin >> arr[i];
 
     // impliment a bubble sort in decanding order
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] < arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+    // final sorted arry
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
     return 0;
 }
