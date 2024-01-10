7fd651470d3c4fbd9954cc2b77cd1d885be68b44
akashydv311 <akashydv311@gmail.com>
2024-01-10 08:16:47 +0530

    insertion sort code added

--- Coder Army/Day-27/sorting/InsertionSort.cpp
+++ Coder Army/Day-27/sorting/InsertionSort.cpp
@@ -13,6 +13,19 @@ int main()
         cin >> arr[i];
 
     // implimenting insertion sort
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+                swap(arr[j], arr[j - 1]);
+        }
+    }
+
+    // final sorted array
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
 
     return 0;
 }
