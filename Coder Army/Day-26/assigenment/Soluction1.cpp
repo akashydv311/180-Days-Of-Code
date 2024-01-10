63ac18d2cf0212cf0f464cafcff07f36bef106e8
akashydv311 <akashydv311@gmail.com>
2024-01-10 06:26:44 +0530

    day-26 added'

---
+++ Coder Army/Day-26/assigenment/Soluction1.cpp
@@ -0,0 +1,18 @@
+// 1: Use Bubble Sort Algorithm to sort the array of integers in decreasing order.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    // values in an array
+    cout << "Enter values in an array: " << endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // impliment a bubble sort in decanding order
+    return 0;
+}
---
+++ Coder Army/Day-26/sorting/BubbleSort.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    // values in an array
+    cout << "Enter values in an array: " << endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // impliment bubble sort
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+    // final array
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
