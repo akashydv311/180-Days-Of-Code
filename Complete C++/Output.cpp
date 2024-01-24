1132f480caf4cab0cbf752ec6f4888f3a98e6e5b
akashydv311 <akashydv311@gmail.com>
2024-01-24 12:55:19 +0530

    output ne line added

---
+++ Complete C++/Output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // output with new line
+
+    // 1st way
+    cout<<"Hello, WOrld!\n";
+    cout<<"I am learning c++";
+    cout<<"\n";
+
+    // 2nd way
+    cout<<"Hello, World!"<<endl;
+    cout<<"I am learning c++";
+
+    return 0;
+}
