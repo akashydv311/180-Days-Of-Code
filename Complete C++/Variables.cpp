4eef208ee2ba3d488b5a46fe3c39720f1b10f6d8
akashydv311 <akashydv311@gmail.com>
2024-01-24 13:25:36 +0530

    Varibale code added

---
+++ Complete C++/Variables.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Multiline comments
+    /*
+    Data Type
+    int, double, char, string, bool
+    */
+
+    // Declare single variable
+    int myNum;
+    myNum = 90;
+    cout<<"My Number is: "<<myNum<<endl;
+
+    // Declare multiple variable
+    int a = 90, b = 78, c = 78;
+    cout<<"Values of a="<<a<<" b="<<b<<" c="<<c<<endl;
+
+    int x, y ,z;
+    x = y = z = 56;
+    cout<<"Values of x="<<x<<" y="<<y<<" z="<<z<<endl;
+
+    return 0;
+}
