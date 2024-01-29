1943516a3639eba45345ea31707349a9207e42bd
akashydv311 <akashydv311@gmail.com>
2024-01-29 17:10:29 +0530

    Cannot make soluction

--- Coder Army/Day-31/assignments/KthMissigPositiveNumber.cpp
+++ Coder Army/Day-31/assignments/KthMissigPositiveNumber.cpp
@@ -7,21 +7,33 @@ Return the kth positive integer that is missing from this array.
 #include <iostream>
 using namespace std;
 
+int missingKthPositiveNumber(int arr[], int n, int target)
+{
+    return -1;
+}
+
 int main()
 {
-    int x;
+    int x, arr[1000], key, n;
 
     // Please create input.txt output.txt
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    // cout << "Enter the number to find square root: ";
-    cin >> x;
+    // cout << "Enter the size of array: ";
+    cin >> n;
+
+    // cout << "Enter values: " << endl;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    // cout << "Enter element to serach in an array: ";
+    cin >> key;
 
     // binary search find the key index in array
-    int ans = squareRoot(x);
+    int index = missingKthPositiveNumber(arr, n, key);
 
-    cout << "Square Root : " << ans << endl;
+    cout << "Missing first positive : " << index << endl;
 
     return 0;
 }
