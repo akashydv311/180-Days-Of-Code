4b03a0700d6307c3433db9a3cf4d107c7a021b63
akashydv311 <akashydv311@gmail.com>
2024-01-30 23:05:53 +0530

    access file

---
+++ Coder Army/Day-37/Access.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    // init
+    vector<double> nums;
+
+    // access
+    // cout << nums.at(0);
+
+    vector<char> ch;
+
+    for (const char &word : ch)
+        cout << word << " ";
+
+    return 0;
+}
