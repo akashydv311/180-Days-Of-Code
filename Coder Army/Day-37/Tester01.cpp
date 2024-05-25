bbbbcb1376928bef820dde9793d03f957b5f5f7f
akashydv311 <akashydv311@gmail.com>
2024-05-25 07:52:20 +0530

    Tester code

--- Coder Army/Day-37/Tester01.cpp
+++ Coder Army/Day-37/Tester01.cpp
@@ -1,11 +1,12 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    vector<int> v(2);
+    vector<int> v;
 
     v.push_back(78);
     v.push_back(67);
@@ -13,6 +14,9 @@ int main()
     v.push_back(67);
 
     cout << v.size();
+    cout << endl;
+
+    cout << binary_search(v.begin(), v.end(), 67) << endl;
 
     return 0;
 }
