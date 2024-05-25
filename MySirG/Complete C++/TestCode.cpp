d3db3e162d0e1b53b27fc10320351f1fa75b4c65
akashydv311 <akashydv311@gmail.com>
2024-05-25 08:05:56 +0530

    MySirG folder added

--- MySirG/Complete C++/TestCode.cpp
+++ MySirG/Complete C++/TestCode.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello, World!");
+    printf("Hello, World !");
     return 0;
 }
 // We can write c code in cpp file becaus c++ is a superset of c langudge
